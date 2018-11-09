2d9a7664b83aca629c6dfc425503833e6b35a672
Artem Belkov <bestk1ngarthur@aol.com>
2018-11-09 18:40:48 +0300

    Initial Commit

---
+++ HackathonApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HackathonApp
+//
+//  Created by a.belkov on 09/11/2018.
+//  Copyright © 2018 bestK1ng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HackathonApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HackathonApp
+//
+//  Created by a.belkov on 09/11/2018.
+//  Copyright © 2018 bestK1ng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
