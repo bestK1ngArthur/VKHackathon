73d6a905bb4d2ca7d50cab4d7d04d0648b633f73
Artem Belkov <bestk1ngarthur@aol.com>
2018-11-10 17:37:22 +0300

    Add "add event" screen

--- HackathonApp/Controllers/AddAchievementController.swift
+++ HackathonApp/Controllers/AddAchievementController.swift
@@ -23,8 +23,7 @@ class AddAchievementController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 1
+        return 7
     }
 
     /*
